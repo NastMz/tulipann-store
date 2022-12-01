af3735f2661c57d0c593759ca18c64f3c97f3237
Kevin Martinez <ksmartinez2307@gmail.com>
2022-12-01 18:03:33 -0500

    Admin dashboard initialized

---
+++ dashboard/src/templates/Main.tsx
@@ -0,0 +1,24 @@
+import {Sidebar} from "../components";
+import {motion} from "framer-motion";
+
+interface MainProps {
+    page: JSX.Element,
+    title: string,
+    pageName?: string,
+}
+
+export const Main = (props: MainProps) => {
+    document.title = props.title;
+
+    return (
+        <div className={"h-screen max-h-screen flex"}>
+            <Sidebar className={'flex-shrink'}/>
+            <main className={'flex flex-col px-12 py-8 flex-grow'}>
+                <h2 className={'font-medium text-3xl flex-shrink mb-8'}>{props.pageName}</h2>
+                <div className={'flex items-center justify-center flex-grow'}>
+                    {props.page}
+                </div>
+            </main>
+        </div>
+    )
+}
