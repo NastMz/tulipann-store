e6d29cd2621cc2fb1912a8e8f386be0bf4922867
Kevin Martinez <ksmartinez2307@gmail.com>
2022-12-08 14:49:56 -0500

    Added whatsapp widget

--- frontend/src/components/WhatsAppFloatIcon.tsx
+++ frontend/src/components/WhatsAppFloatIcon.tsx
@@ -1,10 +1,58 @@
+import {useEffect, useRef, useState} from "react";
+import {WhatsAppWidget} from "./WhatsAppWidget";
+import {AnimatePresence, motion} from "framer-motion";
 import {BsWhatsapp} from "react-icons/all";
 
 export const WhatsAppFloatIcon = (props: { phone: number }) => {
+    const formRef = useRef<any>(null);
+
+    const [formIsOpen, setFormIsOpen] = useState<boolean>(false);
+
+    const hideForm = (e: MouseEvent) => {
+        if (formRef.current && !formRef.current.contains(e.target)) {
+            setFormIsOpen(false);
+        }
+    }
+
+    useEffect(() => {
+        if (formIsOpen) {
+            document.addEventListener('mousedown', hideForm);
+        } else {
+            document.removeEventListener('mousedown', hideForm);
+        }
+    }, [formIsOpen]);
+
     return (
-        <a href={"#"}
-           className="fixed z-30 bottom-5 right-5 rounded-full bg-green-500 shadow-xl p-2 flex items-center justify-center">
-            <BsWhatsapp size={25} color={"white"}/>
-        </a>
+        <div className="fixed z-30 bottom-5 right-5">
+            {!formIsOpen &&
+                <motion.div
+                    initial={{scale: 0}}
+                    animate={{scale: 1}}
+                    exit={{scale: 0}}
+                    transition={{delay: 0.1}}
+                    className="absolute bottom-0 right-0 w-fit h-fit rounded-full bg-green-500 hover:bg-green-700 shadow-2xl p-3 flex items-center justify-center cursor-pointer"
+                    onClick={() => setFormIsOpen(true)}
+                >
+                    <BsWhatsapp size={30} color={"white"}/>
+                </motion.div>
+            }
+            <AnimatePresence>
+                {
+                    formIsOpen &&
+                    <motion.div
+                        initial={{scale: 0}}
+                        animate={{scale: 1}}
+                        exit={{scale: 0}}
+                        className={`z-10 transform origin-bottom-right`}
+                        ref={formRef}
+                    >
+                        <WhatsAppWidget
+                            phone={props.phone}
+                            setIsOpen={setFormIsOpen}
+                        />
+                    </motion.div>
+                }
+            </AnimatePresence>
+        </div>
     )
 }
