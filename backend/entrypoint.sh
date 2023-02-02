b42e0ea145e7cc1810d17f4d0280feba42c0aacd
JhMateo <jmgranada@unillanos.edu.co>
2023-02-01 19:28:06 -0500

    Revert "delete commentary"
    This reverts commit 2e6bf6410b90102136adb498b384a4d47666af42.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 echo 'Make migrations...'
-
 python manage.py makemigrations --settings=tulipann_store.settings.production
 
 echo 'Applying migrations...'
-
 python manage.py migrate --settings=tulipann_store.settings.production
 
-echo 'Running server...'
-
+echo 'Running server...' # deje localhopst que voy a hacer un experimento
 gunicorn --env DJANGO_SETTINGS_MODULE=tulipann_store.settings.production tulipann_store.wsgi:application --bind 0.0.0.0:8000
