fb5c047869b6e392f530f92a3e224fd2a1d0887f
JhMateo <jmgranada@unillanos.edu.co>
2023-02-23 09:47:20 -0500

    Fixed product CRUD for uploading images and put method

--- backend/api/views/crud/ProductCrud.py
+++ backend/api/views/crud/ProductCrud.py
@@ -7,9 +7,9 @@ import uuid
 from api.models import Product, Subcategory, Specification, Image, ProductSubcategory, Feature, Category
 from api.serializers import ProductSerializer, ProductCrudSerializer, SpecificationCrudSerializer, \
     FeatureCrudSerializer, \
-    ImageCrudSerializer, ProductSubcategorySerializer
+    ImageCrudSerializer, ProductSubcategorySerializer, SpecificationSerializer
 from api.utils.authorization_crud import authorization
-from api.utils.image_utils import optimize_and_save_image
+from api.utils.image_utils import optimize_and_save_image, delete_images
 
 
 class ProductList(APIView):
@@ -89,15 +89,59 @@ class ProductCreate(generics.GenericAPIView):
             messages.append('Las características son requeridas')
 
         if 'images' not in request.data:
-            messages.append('Las imágenes son requeridas')
+            messages.append('La información de las imagenes son requeridas')
+
+        if messages:
+            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
 
         if Specification.all_objects.filter(summary=request.data['specification']['summary']).exists():
             messages.append('Esta especificación ya está asignada a otro producto')
+            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+        if 'summary' not in request.data['specification']:
+            messages.append('El resumen de la especificación es requerido')
+            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
 
         subcategories = request.data['subcategories']
         specification = request.data['specification']
         features = request.data['specification']['features']
         images = request.data['images']
+
+        for image in images:
+            if 'src' not in image:
+                messages.append('La imagen es requerida')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            if 'hash' not in image:
+                messages.append('El hash de la imagen es requerido')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+        seen_feature_names = []
+
+        for feature in features:
+            if 'featureName' not in feature:
+                messages.append('El nombre de la caracteristica es requerido.')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            feature_name = feature['featureName']
+
+            if feature_name in seen_feature_names:
+                messages.append(f'El valor de featureName "{feature_name}" está duplicado.')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            seen_feature_names.append(feature_name)
+
+            if 'image' not in feature:
+                messages.append('La información de la imagen para la caracteristica es requerida')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            if 'src' not in feature['image']:
+                messages.append('La imagen de la caracteristica es requerida')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            if 'hash' not in feature['image']:
+                messages.append('El hash de la imagen para la caracteristica es requerido')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
 
         for subcategory in subcategories:
             if not Subcategory.all_objects.filter(id=subcategory['subcategoryId']).exists():
@@ -106,14 +150,13 @@ class ProductCreate(generics.GenericAPIView):
 
             if Subcategory.all_objects.get(id=subcategory['subcategoryId']).category.id != request.data['category']:
                 messages.append('La subcategoría ingresada no corresponde a la categoría del producto')
-
-        if messages:
-            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
 
         new_product = Product.all_objects.get(name=request.data['name'])
 
+        # Create subcategories
         product_subcategory_serializer = ProductSubcategorySerializer()
         for subcategory in subcategories:
             data_product_subcategory = {
@@ -124,15 +167,16 @@ class ProductCreate(generics.GenericAPIView):
                                                          subcategory=data_product_subcategory['subcategory']).exists():
                 product_subcategory_serializer.create(data_product_subcategory)
 
+        # Create specification
         specification_serializer = SpecificationCrudSerializer()
 
         data_specification = {
             'summary': specification['summary'],
             'product': new_product
         }
-
         specification_serializer.create(data_specification)
 
+        # Create features
         db_spec = Specification.all_objects.get(summary=specification['summary'])
 
         feature_serializer = FeatureCrudSerializer()
@@ -149,6 +193,7 @@ class ProductCreate(generics.GenericAPIView):
             }
             feature_serializer.create(data_feature)
 
+        # Create images
         image_serializer = ImageCrudSerializer()
 
         for image in images:
@@ -244,27 +289,164 @@ class ProductUpdate(APIView):
             messages.append('Esta categoría no existe')
             return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
 
+        if 'subcategories' not in request.data:
+            messages.append('Las subcategorías son requeridas')
+
+        if 'specification' not in request.data:
+            messages.append('La especificación es requerida')
+
+        if 'features' not in request.data['specification']:
+            messages.append('Las características son requeridas')
+
+        if 'images' not in request.data:
+            messages.append('La información de las imagenes son requeridas')
+
+        if messages:
+            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+        if 'summary' not in request.data['specification']:
+            messages.append('El resumen de la especificación es requerido')
+            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
         product = Product.all_objects.get(id=id)
-        product_subcategories = ProductSubcategory.all_objects.filter(product=id)
+
+        if Specification.all_objects.filter(summary=request.data['specification']['summary']).exclude(
+                product=product.id).exists():
+            messages.append('Esta especificación ya está asignada a otro producto')
+            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
 
         request.data['category'] = request.data['categoryId']
+        subcategories = request.data['subcategories']
+        specification = request.data['specification']
+        features = request.data['specification']['features']
+        images = request.data['images']
 
-        for product_subcategory in product_subcategories:
-            category_related = Subcategory.all_objects.get(id=product_subcategory.subcategory.id).category
-            if category_related.id != request.data['categoryId']:
-                messages.append('La categoría no corresponde a las subcategorías del producto')
+        for image in images:
+            if 'src' not in image:
+                messages.append('La imagen es requerida')
                 return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            if 'hash' not in image:
+                messages.append('El hash de la imagen es requerido')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+        seen_feature_names = []
+
+        for feature in features:
+            if 'featureName' not in feature:
+                messages.append('El nombre de la caracteristica es requerido.')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            feature_name = feature['featureName']
+
+            if feature_name in seen_feature_names:
+                messages.append(f'El valor de featureName "{feature_name}" está duplicado.')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            seen_feature_names.append(feature_name)
+
+            if 'image' not in feature:
+                messages.append('La información de la imagen para la caracteristica es requerida')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            if 'src' not in feature['image']:
+                messages.append('La imagen de la caracteristica es requerida')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            if 'hash' not in feature['image']:
+                messages.append('El hash de la imagen para la caracteristica es requerido')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+        for subcategory in subcategories:
+            if not Subcategory.all_objects.filter(id=subcategory['subcategoryId']).exists():
+                messages.append('La subcategoría ' + subcategory['subcategoryId'] + ' no existe')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+            if Subcategory.all_objects.get(id=subcategory['subcategoryId']).category.id != request.data['category']:
+                messages.append('La subcategoría ingresada no corresponde a la categoría del producto')
+                return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+        if Product.all_objects.filter(name=request.data['name']).exclude(id=id).exists():
+            messages.append('Este nombre ya está asignado a otro producto')
+            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = ProductSerializer(product, data=request.data)
 
         if not serializer.is_valid():
             return Response({"Errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        if Product.all_objects.filter(name=request.data['name']).exclude(id=id).exists():
-            messages.append('Este nombre ya está asignado a otro producto')
-            return Response({"Errors": messages}, status=status.HTTP_400_BAD_REQUEST)
-
         serializer.save()
+
+        # Update subcategories
+        product_subcategory_serializer = ProductSubcategorySerializer()
+
+        refresh_product_subactegory = ProductSubcategory.all_objects.filter(product=product)
+
+        for prod_subcat in refresh_product_subactegory:
+            prod_subcat.delete()
+
+        for subcategory in subcategories:
+            data_product_subcategory = {
+                'product': product,
+                'subcategory': Subcategory.all_objects.get(id=subcategory['subcategoryId'])
+            }
+            if not ProductSubcategory.all_objects.filter(product=data_product_subcategory['product'],
+                                                         subcategory=data_product_subcategory['subcategory']).exists():
+                product_subcategory_serializer.create(data_product_subcategory)
+
+        # Update specification
+        specification_obj = Specification.all_objects.get(product=product.id)
+
+        data_specification = {
+            'summary': specification['summary'],
+            'product': product.id
+        }
+
+        specification_serializer = SpecificationSerializer(specification_obj, data=data_specification)
+
+        if not specification_serializer.is_valid():
+            return Response({"Errors": specification_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        specification_serializer.save()
+
+        # Update features
+        refresh_feature = Feature.all_objects.filter(specification=specification_obj.id)
+
+        for feature_ref in refresh_feature:
+            delete_images(feature_ref, 'image')
+            feature_ref.delete()
+
+        feature_serializer = FeatureCrudSerializer()
+
+        for feature in features:
+            image_feature = optimize_and_save_image(image_data=feature['image']['src'], object_name='feature')
+            data_feature = {
+                'name': feature['featureName'],
+                'title': feature['title'],
+                'description': feature['description'],
+                'image': image_feature,
+                'hash': feature['image']['hash'],
+                'specification': specification_obj
+            }
+            feature_serializer.create(data_feature)
+
+        # Update images
+        refresh_image = Image.all_objects.filter(product=product.id)
+
+        for image_ref in refresh_image:
+            delete_images(image_ref, 'src')
+            image_ref.delete()
+
+        image_serializer = ImageCrudSerializer()
+
+        for image in images:
+            image_product = optimize_and_save_image(image_data=image['src'], object_name='product')
+            data_image = {
+                'src': image_product,
+                'hash': image['hash'],
+                'product': product
+            }
+            image_serializer.create(data_image)
 
         return Response({
             "Product updated": {
