55338d9799f0f36053c3e2a7ebd7b9695aa06827
Kevin Martinez <ksmartinez2307@gmail.com>
2023-03-02 11:21:14 -0500

    Added a verification for the user who already added a comment to a product, and fixed an error that shows the login alert when the user opens the product page, but has not yet clicked the review button

--- frontend/src/models/interfaces/Commentary.ts
+++ frontend/src/models/interfaces/Commentary.ts
@@ -6,10 +6,12 @@
  * @property {1 | 2 | 3 | 4 | 5} rate - Rating given by the user.
  * @property {string} userName - Name of the user who left the commentary.
  * @property {string} commentary - Text of the commentary.
+ * @property {string} userId - ID of the user who left the commentary.
  */
 export interface Commentary {
   id: string;
   rate: 1 | 2 | 3 | 4 | 5;
   userName: string;
+  userId: string;
   commentary: string;
 }
